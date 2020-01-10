2e79026135c260443c248baf17984f743b72b791
Injoong Yoon <27440940+AttilaTheHen@users.noreply.github.com>
2020-01-09 19:07:00 -0800

    Initial Commit

---
+++ ConcentrationSwift/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ConcentrationSwift
+//
+//  Created by Injoong Yoon on 1/9/20.
+//  Copyright © 2020 Injoong Yoon. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
